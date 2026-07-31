de69acf3d783eb71645ee8fd7c9c545622bed19b
chetan-khachane <khachanechetan94@gmail.com>
2026-07-31 17:05:18 +0530

    HSM-001: Initial repository structure and project documentation

---
+++ src/main/java/com/ys/housing_society_management/HousingSocietyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ys.housing_society_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingSocietyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingSocietyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ys/housing_society_management/HousingSocietyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.housing_society_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingSocietyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
